c0b85f32ead45771aca4d389da506a59011101d2
William Soukkachang <Wsoukkachang@yahoo.com>
2020-01-11 13:03:08 -0600

    Completed ring_buffer. Working on names.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,18 +8,36 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        self.storage[self.current] = item
-        self.current += 1
-        self.current = self.current % len(self.storage)
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        elif self.storage.length == self.capacity:
+            to_delete = self.storage.head
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            if to_delete == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-        for index in self.storage:
-            if index != None:
-                list_buffer_contents.append(index)
+        int_node = self.current
+        list_buffer_contents.append(int_node.value)
+
+        if int_node.next is not None:
+            next_node = int_node.next
+        else:
+            next_node = self.storage.head
+
+        while next_node != int_node:
+            list_buffer_contents.append(next_node.value)
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node = self.storage.head
+
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
